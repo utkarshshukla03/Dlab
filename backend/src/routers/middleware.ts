e4f27e2ac9c9b8c8f3478c401fea2491a42fe92c
Utkarsh Shukla <utkarshshukla372@gmail.com>
2025-07-21 22:03:19 +0530

    Updates in middleware

--- backend/src/routers/middleware.ts
+++ backend/src/routers/middleware.ts
@@ -1,23 +1,63 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 import {JWT_SECRET} from '..';
-export function authMiddleware(req: Request, res: Response, next: NextFunction){
-    const authHeader = req.headers['authorization']?? "";
-  try{
-      const decode = jwt.verify(authHeader, JWT_SECRET);
-        // @ts-ignore
-      if(decode.userId){
-        // @ts-ignore
-        req.userId = decode.userId;
-        return next();
-      }else{
-        return res.status(403).json({
-          message: "You are not logged in"
-        })
-      }
-  }catch(e){
-    return res.status(403).json({
-    message: "You are not logged in"
-    })
-  }
+
+// Extend Express Request interface to include userId
+declare global {
+    namespace Express {
+        interface Request {
+            userId?: number;
+        }
+    }
 }
+
+interface JWTPayload {
+    userId: number;
+}
+
+export function authMiddleware(req: Request, res: Response, next: NextFunction): void {
+    const authHeader = req.headers['authorization'] ?? "";
+    
+    try {
+        const decode = jwt.verify(authHeader, JWT_SECRET) as JWTPayload;
+        
+        if (decode.userId) {
+            req.userId = decode.userId;
+            next();
+        } else {
+            res.status(403).json({
+                message: "You are not logged in"
+            });
+        }
+    } catch (e) {
+        res.status(403).json({
+            message: "You are not logged in"
+        });
+    }
+}
+
+
+
+// import { Request, Response, NextFunction } from "express";
+// import jwt from "jsonwebtoken";
+// import {JWT_SECRET} from '..';
+// export function authMiddleware(req: Request, res: Response, next: NextFunction){
+//     const authHeader = req.headers['authorization']?? "";
+//   try{
+//       const decode = jwt.verify(authHeader, JWT_SECRET);
+//         // @ts-ignore
+//       if(decode.userId){
+//         // @ts-ignore
+//         req.userId = decode.userId;
+//         return next();
+//       }else{
+//         return res.status(403).json({
+//           message: "You are not logged in"
+//         })
+//       }
+//   }catch(e){
+//     return res.status(403).json({
+//     message: "You are not logged in"
+//     })
+//   }
+// }
